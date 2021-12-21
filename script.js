5c81a01595d59aae94db56092e0edc7ddcdd84eb
Yashwanth Mudili <yashwanthmudili@Yashwanths-MacBook-Pro.local>
2021-12-21 15:40:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,142 @@
+$('.quote_msgs').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false
+  });
+
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    // arrows: false
+  });
+
+let hil1 = document.getElementById("highlighter_1"); 
+let hil2 = document.getElementById("highlighter_2");
+let hil3 = document.getElementById("highlighter_3");
+let hil4 = document.getElementById("highlighter_4");
+let hil5 = document.getElementById("highlighter_5");
+let hil6 = document.getElementById("highlighter_6");
+
+function toggleweb(){
+  hil1.classList.remove('hide'); //Imp
+  hil1.classList.add('show');
+
+  hil2.classList.remove('show')
+  hil2.classList.add('hide');
+
+  hil3.classList.remove('show')
+  hil3.classList.add('hide');
+
+  hil4.classList.remove('show');
+  hil4.classList.add('hide');
+
+  hil5.classList.remove('show')
+  hil5.classList.add('hide');
+
+  hil6.classList.remove('show');
+  hil6.classList.add('hide');
+}
+
+function toggleweb2(){
+  hil1.classList.remove('show');
+  hil1.classList.add('hide');
+
+  hil2.classList.remove('hide');//Imp
+  hil2.classList.add('show');
+
+  hil3.classList.remove('show')
+  hil3.classList.add('hide');
+
+  hil4.classList.remove('show');
+  hil4.classList.add('hide');
+
+  hil5.classList.remove('show')
+  hil5.classList.add('hide');
+
+  hil6.classList.remove('show');
+  hil6.classList.add('hide');
+}
+
+function toggleweb3(){
+  hil1.classList.remove('show');
+  hil1.classList.add('hide');
+
+  hil2.classList.remove('show');
+  hil2.classList.add('hide');
+
+  hil3.classList.remove('hide');//Imp
+  hil3.classList.add('show');
+
+  hil4.classList.remove('show');
+  hil4.classList.add('hide');
+
+  hil5.classList.remove('show')
+  hil5.classList.add('hide');
+
+  hil6.classList.remove('show');
+  hil6.classList.add('hide');
+}
+
+function toggleweb4(){
+  hil1.classList.remove('show');
+  hil1.classList.add('hide');
+
+  hil2.classList.remove('show');
+  hil2.classList.add('hide');
+
+  hil3.classList.remove('show')
+  hil3.classList.add('hide');
+
+  hil4.classList.remove('hide');//Imp
+  hil4.classList.add('show');
+
+  hil5.classList.remove('show')
+  hil5.classList.add('hide');
+
+  hil6.classList.remove('show');
+  hil6.classList.add('hide');
+}
+
+function toggleweb5(){
+  hil1.classList.remove('show');
+  hil1.classList.add('hide');
+
+  hil2.classList.remove('show');
+  hil2.classList.add('hide');
+
+  hil3.classList.remove('show')
+  hil3.classList.add('hide');
+
+  hil4.classList.remove('show');
+  hil4.classList.add('hide');
+
+  hil5.classList.remove('hide');//Imp
+  hil5.classList.add('show');
+
+  hil6.classList.remove('show');
+  hil6.classList.add('hide');
+}
+
+function toggleweb6(){
+  hil1.classList.remove('show');
+  hil1.classList.add('hide');
+
+  hil2.classList.remove('show');
+  hil2.classList.add('hide');
+
+  hil3.classList.remove('show')
+  hil3.classList.add('hide');
+
+  hil4.classList.remove('show');
+  hil4.classList.add('hide');
+
+  hil5.classList.remove('show')
+  hil5.classList.add('hide');
+
+  hil6.classList.remove('hide');//Imp
+  hil6.classList.add('show');
+}
